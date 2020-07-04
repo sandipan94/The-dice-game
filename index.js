c5521c5dbf3586a279c3b628eb13e9c55da4b78f
sandipan bera <39095054+sandipan94@users.noreply.github.com>
2020-07-04 09:52:11 +0530

    full website updated

---
+++ index.js
@@ -0,0 +1,52 @@
+
+var randomNumber1= Math.ceil(Math.random()*6);
+var randomNumber2= Math.ceil(Math.random()*6);
+
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").textContent="player 1 win!";
+
+}else if(randomNumber1<randomNumber2){
+  document.querySelector("h1").textContent="player 2 win!";
+}else{
+  document.querySelector("h1").textContent="Draw!";
+}
+
+if(randomNumber2===1){
+  document.querySelector(".img2").setAttribute("src","images/dice1.png");
+}
+if(randomNumber2===2){
+  document.querySelector(".img2").setAttribute("src","images/dice2.png");
+}
+if(randomNumber2===3){
+  document.querySelector(".img2").setAttribute("src","images/dice3.png");
+}
+if(randomNumber2===4){
+  document.querySelector(".img2").setAttribute("src","images/dice4.png");
+}
+if(randomNumber2===5){
+  document.querySelector(".img2").setAttribute("src","images/dice5.png");
+}
+if(randomNumber2===6){
+  document.querySelector(".img2").setAttribute("src","images/dice6.png");
+}
+
+
+
+if(randomNumber1===1){
+  document.querySelector(".img1").setAttribute("src","images/dice1.png");
+}
+if(randomNumber1===2){
+  document.querySelector(".img1").setAttribute("src","images/dice2.png");
+}
+if(randomNumber1===3){
+  document.querySelector(".img1").setAttribute("src","images/dice3.png");
+}
+if(randomNumber1===4){
+  document.querySelector(".img1").setAttribute("src","images/dice4.png");
+}
+if(randomNumber1===5){
+  document.querySelector(".img1").setAttribute("src","images/dice6.png");
+}
+if(randomNumber===6){
+  document.querySelector(".img1").setAttribute("src","images/dice6.png");
+}
